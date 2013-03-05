19bd2540fb40a5a360faa44121fcac6abc9540df
Learnen Towebapp <learnentowebapp@gmail.com>
2013-03-04 23:47:13 -0800

    Finish static pages chapter

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ group :development do
 end
 
 group :test do
-	gem 'capybara'
+	gem 'capybara', '= 1.1.2'
 	gem 'guard'
 	gem 'terminal-notifier-guard'
 	gem 'rb-fsevent'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,8 +5,40 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/pages/home'
-      page.should have_content('Sample App')
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Sample App | Home")
     end
   end
+
+  describe "Help page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Sample App | Help")
+    end
+  end
+
+  describe "About page" do
+
+     it "should have the content 'Sample App'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Sample App | About")
+    end
+  end
+
 end
